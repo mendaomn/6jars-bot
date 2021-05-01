a41b05a0c39151c5458cfaa6c6ba19ffe161107d
Alessandro Menduni <menduni6@gmail.com>
2021-05-01 13:40:35 +0200

    fix: setupLocale throwing on multiple runs

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,26 +1,28 @@
 import Numeral from "numeral";
 
 export function setupLocale(numeral: typeof Numeral) {
-  numeral.register("locale", "it", {
-    delimiters: {
-      thousands: ".",
-      decimal: ",",
-    },
-    abbreviations: {
-      thousand: "mila",
-      million: "mil",
-      billion: "b",
-      trillion: "t",
-    },
-    ordinal: function (number) {
-      return "º";
-    },
-    currency: {
-      symbol: "€",
-    },
-  });
+  if (numeral.locale() !== "it") {
+    numeral.register("locale", "it", {
+      delimiters: {
+        thousands: ".",
+        decimal: ",",
+      },
+      abbreviations: {
+        thousand: "mila",
+        million: "mil",
+        billion: "b",
+        trillion: "t",
+      },
+      ordinal: function (number) {
+        return "º";
+      },
+      currency: {
+        symbol: "€",
+      },
+    });
 
-  numeral.locale("it");
+    numeral.locale("it");
+  }
 
   return numeral;
 }
